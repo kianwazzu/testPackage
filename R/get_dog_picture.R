5fe0ac6014df1fccfe4519f2f95a2124806fdd7e
kianwazzu <kian.ankerson@wsu.edu>
2021-11-04 16:23:52 -0700

    First commit, dog pic command

--- R/get_dog_picture.R
+++ R/get_dog_picture.R
@@ -1,4 +1,15 @@
 #get dog http
+#' Get dog picture
+#'
+#' Get a random picture of a dog and display it
+#'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' get_dog_picture()
+#'
 get_dog_picture <- function(){
 response <- httr::GET("https://dog.ceo/api/breeds/image/random")
 if(httr::status_code(response) == 200){
